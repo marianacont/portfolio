ff0389990469ccc89185461d5b1d0e87c14dbf9a
marianacont <marianacontipeycere@gmail.com>
2023-01-28 09:39:37 -0300

    armado de slider par proyectos

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,17 +1,42 @@
-import Swiper from './swiper-bundle.min.js'
+const card = document.querySelectorAll('.card');
+const prev = document.querySelectorAll('.icon-prev');
+const next = document.querySelectorAll('.icon-next');
+const indicators = document.querySelectorAll('.indicators button');
 
-const swiper = new Swiper('.slide-content', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    }
+
+prev.forEach(b => b.addEventListener('click', () =>{
+    setElement(card, 'icon-prev');
+    
 })
+)
+next.forEach(b, i => b.addEventListener('click', () =>{
+    setElement(card, 'icon-next');
+    
+})
+)
+
+function getElement (elements){Mn 
+    let newItem
+    for (el of elements){
+        if(el.classList.conatins(className)){
+            el.classList.remove(className);
+            newItem = el
+        }
+    }
+    return newItem
+}
+
+// Permite pasar de un elemento al siguiente, al anterior, al primero o al último, según el nombre de su clase
+function setElement(array, type){
+    let el = getElement(array);
+
+    let newItem = (
+        type === 'icon-prev' ?
+        (el.previusElementSibling || el.parentNode.lastElementChild) :
+        type == 'icon.next' ?
+        (el.nextElementSibling || el.parentNode.firstElementChild) :
+        (el)
+        )
+        newItem.classList.add(className)
+
+}
